b596efa2bd9b2b280f02a296d84ea244a4447657
tfoliveira <tfoliveira11@gmail.com>
2023-12-12 09:12:37 -0300

    refactor: rename files

--- src/main/java/com/example/paymentapi/PaymentApiApplication.java
+++ src/main/java/com/example/paymentapi/PaymentApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.java21features;
+package com.example.paymentapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java21featuresApplication {
+public class PaymentApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Java21featuresApplication.class, args);
+		SpringApplication.run(PaymentApiApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/paymentapi/PaymentApiApplicationTests.java
+++ src/test/java/com/example/paymentapi/PaymentApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.java21features;
+package com.example.paymentapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Java21featuresApplicationTests {
+class PaymentApiApplicationTests {
 
 	@Test
 	void contextLoads() {
