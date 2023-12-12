bd021f0d10afe3519d5d583e173a99801f12a9d0
tfoliveira <tfoliveira11@gmail.com>
2023-12-12 09:02:17 -0300

    init config

---
+++ src/main/java/com/example/java21features/Java21featuresApplication.java
@@ -0,0 +1,13 @@
+package com.example.java21features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21featuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java21featuresApplication.class, args);
+	}
+
+}
